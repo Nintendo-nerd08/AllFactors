6bbbf23142dda015821922e036297d5b6e5a2a45
lloydkev000 <lloydkevin631@gmail.com>
2024-09-20 13:58:05 -0400

    All done!

--- src/AllFactors.java
+++ src/AllFactors.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class AllFactors {
+public class AllFactors {
+
+    public static void main(String[] args) {
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+
+    }
+
+    private static void printFactors(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
 }
+
